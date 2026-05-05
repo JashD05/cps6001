869a2d6d727d345c94e8f602024ea44bc217ba2a
Jash <2303766@live.stmarys.ac.uk>
2026-05-05 10:01:15 +0100

    CI: Align Go version and enable TypeScript parser for ESLint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -20,6 +20,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
     ecmaFeatures: {
       jsx: true,
     },
